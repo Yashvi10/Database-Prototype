0b28c141620367750d02aedee54a5dd2ea9e7683
Yashvi <ys472108@dal.ca>
2021-08-03 14:27:49 +0530

    added required class

--- src/main/java/logs/QueryError.java
+++ src/main/java/logs/QueryError.java
@@ -3,13 +3,12 @@ package logs;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class NotExistRecord implements EventLogger{
+public class QueryError implements EventLogger{
 
   @Override
   public void event(String name, long timeELapsed) throws IOException {
-
     FileWriter fileWriter = new FileWriter("databases/EventLogs/log.txt", true);
-    fileWriter.write(name + "\tSchema or table does not exist!" + " " + "Executed in " + timeELapsed / 1000000 + "ms"+"\n");
+    fileWriter.write(name + "\tIncorrect Query!" + " " + "Executed in " + timeELapsed / 1000000 + "ms" + "\n");
     fileWriter.close();
   }
 
